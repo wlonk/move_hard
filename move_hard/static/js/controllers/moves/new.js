7406151efe7a7e5067ddd733359a09885a7e05c2
Kit La Touche <kit.la.t@gmail.com>
2014-04-22 22:31:23 -0600

    Adds game dropdown to new move form.

--- move_hard/static/js/controllers/moves/new.js
+++ move_hard/static/js/controllers/moves/new.js
@@ -1,13 +1,15 @@
 App.MovesNewController = Ember.ObjectController.extend({
   needs: ['auth'],
+  games: function () {
+    // @todo this should have a null option at the beginning.
+    return this.store.find('game');
+  }.property(),
   actions: {
     save: function () {
       var self = this;
-      var user = 1;  // Obviously, this is bad.
-      var game = 1;  // Obviously, this is bad.
+      var user = 1;  // @todo: this should use the current user, and get checked by the server.
       var model = this.get('model');
       model.set('user', user);
-      model.set('game', game);
       model.save().then(
         function (saved_model) {
           self.transitionToRoute('move', saved_model.get('id'));
